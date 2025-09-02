e2083b1ea7325006cb80a0f0905b6cbb9c780359
t3knical <t3chnicald3ath@gmail.com>
2025-09-02 00:55:08 -0400

    Create repo.js

---
+++ repo.js
@@ -0,0 +1,26 @@
+[
+  {
+    "Author": "T3knical",
+    "Name": "Hunter",
+    "Punchline": "Auto-execute configured emotes as you chat!",
+    "Description": "Tracks item collection progress and targets relevant mobs",
+    "InternalName": "Hunter",
+    "AssemblyVersion": "1.0.0",
+    "ApplicableVersion": "any",
+    "Tags": [
+      "Hunt",
+      "Gather",
+      "Monster Killer"
+    ],
+    "DalamudApiLevel": 13,
+    "LoadPriority": 90,
+    "IconUrl": "https://raw.githubusercontent.com/t3knical/Hunter/main/plugin_icon.png",
+    "RepoUrl": "https://github.com/t3knical/Hunter",
+    "IsHide": "False",
+    "IsTestingExclusive": "True",
+    "DownloadCount": 1,
+    "LastUpdate": 1618608322,
+    "DownloadLinkInstall": "https://github.com/t3knical/Hunter/raw/refs/heads/main/latest.zip",
+    "DownloadLinkUpdate": "https://github.com/t3knical/Hunter/raw/refs/heads/main/latest.zip"
+  }
+]
